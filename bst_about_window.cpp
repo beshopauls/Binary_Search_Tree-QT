2aa3ef679a10679132ed088dd0f8891d3b92b486
beshopauls <73366045+beshopauls@users.noreply.github.com>
2021-05-24 08:50:49 +0500

    Add files via upload

---
+++ bst_about_window.cpp
@@ -0,0 +1,36 @@
+#include "bst_about_window.h"
+#include <QTextEdit>
+
+BST_About_Window::BST_About_Window()
+{
+  /* window = new QMainWindow();
+    window->setWindowTitle("About BST Visualization");
+    window->setFixedSize(QSize(350, 300));
+    labelLayout = new QVBoxLayout;
+    QVBoxLayout *textAreaLayout = new QVBoxLayout();
+    containerLayout = new QHBoxLayout;
+    containerLayout->addLayout(labelLayout);
+    QVBoxLayout *vContainerLayout = new QVBoxLayout();
+    vContainerLayout->addLayout(containerLayout);
+    vContainerLayout->addLayout(textAreaLayout);
+    container = new QWidget;
+    container->setLayout(vContainerLayout);
+    // Create the main layout and add all the widgets to it
+    mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(container);
+    // mainLayout->addLayout(buttonLayout); // layout for ok/close button
+    centralWidget = new QWidget(window);
+    centralWidget->setLayout(mainLayout);
+    window->setCentralWidget(centralWidget);*/
+}
+
+BST_About_Window::~BST_About_Window()
+{
+    /*delete labelLayout;
+    delete containerLayout;
+    delete container;
+    delete mainLayout;
+    delete centralWidget;
+    delete window;*/
+}
+
---
+++ bst_about_window.h
@@ -0,0 +1,28 @@
+#ifndef BST_ABOUT_WINDOW_H
+#define BST_ABOUT_WINDOW_H
+
+#include "binarysearchtree.h"
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+
+class BST_About_Window
+{
+public:
+    BST_About_Window();
+    ~BST_About_Window();
+
+private:
+    /*QMainWindow *window;
+    QVBoxLayout *labelLayout;
+    QHBoxLayout *containerLayout;
+    QWidget *container;
+    QVBoxLayout *mainLayout;
+    QWidget *centralWidget;*/
+
+};
+
+#endif // BST_ABOUT_WINDOW_H
